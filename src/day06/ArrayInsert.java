00c3e09c3e342fc004e4a9b3f37ccdd91edc78fa
leemhyemin <leeta6824@naver.com>
2021-12-08 23:12:38 +0900

    [21.12.08]

---
+++ src/day06/ArrayInsert.java
@@ -0,0 +1,50 @@
+package day06;
+
+import java.util.Arrays;
+
+public class ArrayInsert {
+
+    public static void main(String[] args) {
+
+        int[] arr = {50, 150, 250, 300};
+        // 50 과 150 사이에 100추가하기.
+
+        System.out.println("추가 전: "+ Arrays.toString(arr));
+
+        int newNumber = 100; //신규 삽입데이터
+        int targetIndex = 1; // 삽입 위치
+
+        // 1. 사이즈가 하나 더 큰 배열 생성
+        int[] temp = new int[arr.length + 1];
+
+        // 2. 기존데이터를 전부복사
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+            // 왼쪽이 공간 오른쪽이 값. 복사개념
+        }
+        System.out.println("추가 후: "+ Arrays.toString(temp));
+
+        // 3. 끝데이터부터 뒤로 밀기
+
+        for (int i = temp.length-1; i > targetIndex; i--) {
+            temp[i] = temp[i-1];
+        }
+
+//            temp[4] = temp[3];
+//            temp[3] = temp[2];
+//            temp[2] = temp[1];
+//            temp[1] = temp[0];
+        // 4. 타겟인덱스에 데이터 추가
+        temp[targetIndex] = newNumber;
+
+        // 5. 주소변경
+        arr = temp;
+        temp = null;
+
+
+
+
+
+
+    } // end main
+} //end class
---
+++ src/day06/tvxqQuiz.java
@@ -0,0 +1,61 @@
+package day06;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class tvxqQuiz {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] tvxq = {"영웅재중", "최강창민", "시아준수", "믹키유천", "유노윤호"};
+
+        System.out.println("* 삭제 전 정보: " + Arrays.toString(tvxq));
+
+        // 1. 삭제 타겟의 이름을 입력받는다.
+        // 2. 해당 이름이 있는지 탐색해본다.
+        // 3. 탐색에 성공하면 해당 이름의 인덱스를 알아온다.
+        // 4. 탐색에 실패하면 실패한 증거를 확보한다.
+        // 5. 성공했을 시 삭제
+        // 6. 위 내용을 삭제가 정확히 완료될때까지 반복한다.
+
+        while (true) {
+            System.out.println("- 삭제할 학생의 별명을 입력하세요.");
+            System.out.printf(">> ");
+            String targetName = sc.next();
+
+            //탐색 알고리즘
+            int index = -1;
+            for (int i = 0; i < tvxq.length; i++) {
+                if (targetName.equals(tvxq[i])) {
+                    index = i;
+                    break;
+                }
+            }
+//탐색
+
+            for (int i = 0; i < tvxq.length-1; i++) {
+                tvxq[i] = tvxq[i+1];
+            }
+
+            String[] temp = new String[tvxq.length-1];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = tvxq[i];
+            }
+
+            //삭제 여부 판단
+            if (index != -1) {
+                System.out.printf("%s의 별명을 삭제합니다.\n", targetName);
+                String newName = sc.next();
+                tvxq[index] = newName;
+                System.out.println("삭제 완료!!");
+                System.out.println("* 삭제 후 정보: " + Arrays.toString(tvxq));
+                break;
+            } else {
+                System.out.printf("해당 별명 (%s)은 존재하지 않습니다.\n", targetName);
+
+            } // end if
+        } // end while
+    } // end main
+} // end class
